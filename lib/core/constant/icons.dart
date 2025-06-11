82c1590614cccf81c1d4d2fdb031fc4651783be7
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-06-11 16:08:55 +0600

    bottom nav bar color changed

--- lib/core/constant/icons.dart
+++ lib/core/constant/icons.dart
@@ -1,4 +1,13 @@
 class AppIcons{
   static const String iconPath = 'assets/icons';
   static const String voiceAi = '$iconPath/voice_ai.png';
+  static const String weatherOutline = '$iconPath/weather_outline.svg';
+  static const String weatherFill = '$iconPath/weather_fill.svg';
+  static const String pilotLogBookOutline = '$iconPath/pilot_log_book_outline.svg';
+  static const String pilotLogBookFill = '$iconPath/pilot_log_book_fill.svg';
+  static const String podcastOutline = '$iconPath/podcast_outline.svg';
+  static const String podcastFill = '$iconPath/podcast_fill.svg';
+  static const String eBookOutline = '$iconPath/e-book_outline.svg';
+  static const String eBookFill = '$iconPath/e-book_fill.svg';
+  static const String voiceAiSvg = '$iconPath/voice_ai.svg';
 }
