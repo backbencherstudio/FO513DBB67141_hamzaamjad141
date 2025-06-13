81ec99159db5c0adb7b5a760c8f14a1881c3fe65
MuntasirAsif <muntasirashifee@gmail.com>
2025-06-13 08:54:03 +0600

    Fix appbar safe area

--- lib/features/ebook_screen/presentation/ebook_screen.dart
+++ lib/features/ebook_screen/presentation/ebook_screen.dart
@@ -15,87 +15,103 @@ class EbookScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
     return CreateScreen(
-      child: SafeArea(
-        child: Padding(
-          padding: AppPadding.screenHorizontal,
-          child: Column(
-            children: [
-              SizedBox(height: 20.h),
-              EBookAppBar(textTheme: textTheme),
-              SizedBox(height: 32.h),
-              Text('E-book', style: textTheme.headlineSmall),
+      child: Padding(
+        padding: AppPadding.screenHorizontal,
+        child: Column(
+          children: [
+            EBookAppBar(textTheme: textTheme),
+            SizedBox(height: 32.h),
+            Text('E-book', style: textTheme.headlineSmall),
 
-              /*Expanded(
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 8.0,
-                    mainAxisSpacing: 8.0,
-                    childAspectRatio: 0.75,
-                  ),
-                  itemCount: EbookList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final data = EbookList[index];
-                    EBook book = EBook.fromJson(data);
+            /*Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                  childAspectRatio: 0.75,
+                ),
+                itemCount: EbookList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final data = EbookList[index];
+                  EBook book = EBook.fromJson(data);
 
-                    return Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.grey[200],
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.grey[200],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: SizedBox(
+                            height: 150,  // Fixed height
+                            width: double.infinity,  // Full width
                             child: Image.network(
                               book.imageUrl,
                               fit: BoxFit.cover,
-                              height: 150,
-                              width: double.infinity,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 8.0,
-                            ),
-                            child: Text(
-                              book.bookTitle,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 8.0,
-                            ),
-                            child: Row(
-                              children: [
-                                Text(book.bookId),
-                                Spacer(),
-                                LinearProgressIndicator(
-                                  value: book.progress,
-                                  backgroundColor: Colors.grey[300],
-                                  valueColor: AlwaysStoppedAnimation(
-                                    Colors.blue,
+                              loadingBuilder: (context, child, loadingProgress) {
+                                if (loadingProgress == null) {
+                                  return child; // Image loaded successfully
+                                } else {
+                                  return Center(
+                                    child: CircularProgressIndicator(
+                                      value: loadingProgress.expectedTotalBytes != null
+                                          ? loadingProgress.cumulativeBytesLoaded /
+                                          (loadingProgress.expectedTotalBytes ?? 1)
+                                          : null,
+                                    ),
+                                  );
+                                }
+                              },
+                              errorBuilder: (context, error, stackTrace) {
+                                return Center(
+                                  child: Icon(
+                                    Icons.error,
+                                    color: Colors.red,
+                                    size: 40,
                                   ),
-                                ),
-                              ],
+                                );
+                              },
                             ),
                           ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),*/
-            ],
-          ),
+                        ),
+                        SizedBox(height: 8),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text(
+                            book.bookTitle,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Row(
+                            children: [
+                              Text(book.bookId),
+                              Spacer(),
+                              LinearProgressIndicator(
+                                value: book.progress,
+                                backgroundColor: Colors.grey[300],
+                                valueColor: AlwaysStoppedAnimation(Colors.blue),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),*/
+          ],
         ),
       ),
     );
--- lib/features/ebook_screen/presentation/widgets/e_book_app_bar.dart
+++ lib/features/ebook_screen/presentation/widgets/e_book_app_bar.dart
@@ -15,30 +15,62 @@ class EBookAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      spacing: 12.w,
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(100),
-          child: Image.network(
-            'https://i.pravatar.cc/150?img=12',
-            height: 47.h,
-            width: 47.h,
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(height: 20.h),
+          Row(
+            spacing: 12.w,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(
+                  'https://i.pravatar.cc/150?img=12',
+                  height: 47.h,
+                  width: 47.h,
+                  fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress == null) {
+                      return child; // Image loaded successfully
+                    } else {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                              (loadingProgress.expectedTotalBytes ?? 1)
+                              : null,
+                        ),
+                      );
+                    }
+                  },
+                  errorBuilder: (context, error, stackTrace) {
+                    // Fallback widget in case of error
+                    return Center(
+                      child: Icon(
+                        Icons.error,
+                        color: Colors.red,
+                        size: 40,
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Good Morning,',
+                    style: textTheme.labelLarge!.copyWith(color: AppColors.primary),
+                  ),
+                  Text('Ronald Richards'),
+                ],
+              ),
+              const Spacer(),
+              CommonWidget.secondaryButton(child: SvgPicture.asset(AppIcons.love)),
+            ],
           ),
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Good Morning,',
-              style: textTheme.labelLarge!.copyWith(color: AppColors.primary),
-            ),
-            Text('Ronald Richards'),
-          ],
-        ),
-        const Spacer(),
-        CommonWidget.secondaryButton(child: SvgPicture.asset(AppIcons.love)),
-      ],
+        ],
+      ),
     );
   }
 }
