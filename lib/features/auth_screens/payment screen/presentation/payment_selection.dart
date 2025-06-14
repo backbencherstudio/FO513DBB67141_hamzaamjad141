f8454e598c1ff52e93c1bd4df82a18057236566f
df <fd>
2025-06-14 17:03:43 +0600

    auth done

--- lib/features/auth_screens/payment screen/presentation/payment_selection.dart
+++ lib/features/auth_screens/payment screen/presentation/payment_selection.dart
@@ -1,11 +1,13 @@
 import 'package:aviation_app/core/constant/icons.dart';
 import 'package:aviation_app/core/constant/padding.dart';
 import 'package:aviation_app/core/routes/route_name.dart';
+import 'package:aviation_app/core/utils/common_widget/common_widget.dart';
 import 'package:aviation_app/core/utils/utils.dart';
 import 'package:aviation_app/features/auth_screens/payment%20screen/presentation/widgets/custom_select_tile.dart';
 import 'package:aviation_app/features/create_screen/create_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 
 class PaymentSelection extends StatelessWidget {
@@ -19,7 +21,11 @@ class PaymentSelection extends StatelessWidget {
         padding: AppPadding.screenHorizontal,
         child: Column(
         children: [
-          SizedBox(height: 160.h,),
+          SizedBox(height: 55.h,),
+          Align(
+          alignment: Alignment.centerLeft,
+          child: CommonWidget.secondaryButton(child: SvgPicture.asset(AppIcons.backIcon) ,onTap: (){ Navigator.pop(context);})),
+          SizedBox(height: 50.h,),
           Text("Choose Payment\n option",
           textAlign: TextAlign.center,
           style: style.headlineSmall!.copyWith(
