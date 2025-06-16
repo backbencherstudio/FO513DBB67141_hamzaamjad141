4acea2967351072a2cda99573ceddee54c7992cd
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-06-16 10:55:03 +0600

    log book summary screen done

--- lib/features/pilot_log_book/presentation/widgets/log_book_summary/log_book_summary.dart
+++ lib/features/pilot_log_book/presentation/widgets/log_book_summary/log_book_summary.dart
@@ -1,14 +1,18 @@
+import 'package:aviation_app/core/constant/icons.dart';
 import 'package:aviation_app/core/theme/theme_extension/app_colors.dart';
+import 'package:aviation_app/core/utils/common_widget/primary_button/primary_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:hive_flutter/adapters.dart';
+import '../../../riverpod/log_book_notifier.dart';
+import 'log_book_summary_card.dart';
 
 class LogBookSummary extends StatelessWidget {
   const LogBookSummary({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return Container(
       padding: EdgeInsets.all(16.r),
       decoration: BoxDecoration(
@@ -16,42 +20,51 @@ class LogBookSummary extends StatelessWidget {
         borderRadius: BorderRadius.circular(8.r),
         border: Border.all(color: Colors.white.withValues(alpha: 0.1))
       ),
-      child: GridView.builder(
-        itemCount: 8,
-        physics: NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisSpacing: 18.h,
-          crossAxisSpacing: 18.w
-        ),
-        itemBuilder: (_, index){
-          return Stack(
-            fit: StackFit.expand,
-            children: [
-              Container(
-                width: 139.w,
-                height: 122.h,
-                decoration: BoxDecoration(
-                  color: AppColors.secondary,
-                  borderRadius: BorderRadius.circular(8.r),
-                  border: Border.all(color: Colors.white.withValues(alpha: 0.2
-                  ),),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withValues(alpha:0.2),
-                      offset: Offset(0, 4),
-                      spreadRadius: 2.r,
-                      blurRadius: 8.r,
-                    )
-                  ]
-                  //image: DecorationImage( image: DecoratedI 'assets/log_book_background.png',)
+      child: Column(
+        children: [
+          Text("Logbook Summary",style: textTheme.headlineSmall,),
+          SizedBox(height: 16.h,),
+          Consumer(
+            builder: (_, ref, _) {
+              final logBookSummaryList = ref.read(logBookProvider.notifier).userLogBookSummaryList;
+              return GridView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: logBookSummaryList.length,
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 18.h,
+                  crossAxisSpacing: 18.w
                 ),
-              ),
-            
-            ],
-          );
-        },
+                itemBuilder: (_, index){
+                  final item = logBookSummaryList[index];
+                  return  LogBookSummaryCard(
+                      title: item["title"],
+                      quantity: item["quantity"],
+                      svgIconPath: item["iconPath"],
+                    );
+                },
+              );
+            }
+          ),
+          SizedBox(height: 18.h,),
+          IntrinsicHeight(
+            child: SizedBox(
+              width: double.infinity,
+              child: LogBookSummaryCard(
+                  svgIconPath: AppIcons.global, title: "Cross country", quantity: 00),
+            ),
+          ),
+          SizedBox(height: 18.h,),
+          PrimaryButton(
+            onTap: (){
+              debugPrint("\nTuki 😉\n");
+            },
+            bodyText: "Download Logs CSV",
+          )
+
+        ],
       ),
     );
   }
