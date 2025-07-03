7326ecfaf8f9b918f547006e7d961508a3a7515e
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-07-03 16:32:11 +0600

    google sign in with api

--- lib/features/auth_screens/auth_provider/auth_state.dart
+++ lib/features/auth_screens/auth_provider/auth_state.dart
@@ -1,13 +1,26 @@
 import 'package:aviation_app/features/auth_screens/model/google_user_model.dart';
+import 'package:aviation_app/features/auth_screens/model/user_model.dart';
 
 class AuthState {
   final GoogleUserModel? googleUser;
-  AuthState({this.googleUser});
-  AuthState copyWith({GoogleUserModel? googleUser}) {
-    return AuthState(googleUser: googleUser ?? this.googleUser);
+  final UserModel? user;
+  final String? userToken;
+
+  AuthState({this.googleUser, this.user, this.userToken});
+
+  AuthState copyWith({
+    GoogleUserModel? googleUser,
+    UserModel? user,
+    String? userToken,
+  }) {
+    return AuthState(
+      googleUser: googleUser ?? this.googleUser,
+      user: user ?? this.user,
+      userToken: userToken ?? this.userToken,
+    );
   }
 
-  AuthState clearGoogleUser() {
-    return AuthState(googleUser: null);
+  AuthState clearUser() {
+    return AuthState(googleUser: null, user: null, userToken: null);
   }
 }
