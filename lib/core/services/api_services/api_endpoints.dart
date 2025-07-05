c00f4b421657d16d56364d4b0ea7dbe8d7006cea
df <fd>
2025-07-05 08:07:28 +0600

    push to new branch

--- lib/core/services/api_services/api_endpoints.dart
+++ lib/core/services/api_services/api_endpoints.dart
@@ -2,12 +2,10 @@ class ApiEndPoints {
 
   static const String baseUrl = "http://192.168.4.3:3000";
   static const String googleLogin = 'users/google-login';
-<<<<<<< HEAD
   static const String login = 'users/login';
   static const String signUp = 'users/register';
   static const String signUpOtp = 'users/registerVerify';
   static const String sendOtp = 'users/forgetPassword';
-=======
+  
   static const String podcast = '$baseUrl/podcasts';
->>>>>>> 70c57a6f70bc9806f23b2cf7f0b389d3574048aa
 }
