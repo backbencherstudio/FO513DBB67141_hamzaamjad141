4d7941db17630b13bc2810fcd0eb1eb8362eae7c
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-07-06 19:23:49 +0600

    profile header done

--- lib/features/profile_screen/presentation/widgets/profile_screen_header.dart
+++ lib/features/profile_screen/presentation/widgets/profile_screen_header.dart
@@ -1,7 +1,9 @@
 import 'package:aviation_app/core/constant/icons.dart';
 import 'package:aviation_app/core/constant/images.dart';
 import 'package:aviation_app/core/theme/theme_extension/app_colors.dart';
+import 'package:aviation_app/features/auth_screens/auth_provider/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
@@ -39,7 +41,37 @@ class ProfileScreenHeader extends StatelessWidget {
           child: Stack(
             fit: StackFit.expand,
             children: [
-              Image.asset(AppImages.men, fit: BoxFit.contain),
+              Consumer(
+                builder: (_, ref, _) {
+                  final userImage = ref.watch(authProvider).user!.image;
+                  return   ClipOval(
+                    child: Image.network(
+                      userImage?? "",
+                      fit: BoxFit.cover,
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if (loadingProgress == null) {
+                          return child; // Image loaded successfully
+                        } else {
+                          return Center(
+                            child: CircularProgressIndicator(
+                              value: loadingProgress.expectedTotalBytes != null
+                                  ? loadingProgress.cumulativeBytesLoaded /
+                                  (loadingProgress.expectedTotalBytes ?? 1)
+                                  : null,
+                            ),
+                          );
+                        }
+                      },
+                      errorBuilder: (context, error, stackTrace) {
+                        // Fallback widget in case of error
+                        return Center(
+                          child: Icon(Icons.image_not_supported_outlined,  size: 40.sp),
+                        );
+                      },
+                    ),
+                  );
+                }
+              ),
               Align(
                 alignment: Alignment.bottomRight,
                 child: GestureDetector(
@@ -52,7 +84,12 @@ class ProfileScreenHeader extends StatelessWidget {
         ),
 
         /// User Name
-        Text("Nahidul Islam Shakin",style:Theme.of(context).textTheme.headlineSmall),
+        Consumer(
+            builder: (_, ref, _) {
+              final userName= ref.watch(authProvider).user!.name;
+            return Text(userName,style:Theme.of(context).textTheme.headlineSmall);
+          }
+        ),
 
         Divider(color: AppColors.secondaryTextColor.withValues(alpha: 0.4),)
       ],
