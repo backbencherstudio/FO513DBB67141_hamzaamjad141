04a316d266ad02c5d91e7975c156f33235101c41
MuntasirAsif <muntasirashifee@gmail.com>
2025-07-13 16:14:32 +0600

    feat: ai voice assistant voice reply

--- lib/features/voice_ai_screen/presentation/widget/text_field_toggle.dart
+++ lib/features/voice_ai_screen/presentation/widget/text_field_toggle.dart
@@ -7,12 +7,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 import '../../data/provider/text_filed_to_voice.dart';
 
 class TextFieldToggle extends ConsumerWidget {
-  final String svgAsset; // Path to SVG asset (e.g., AppIcons.keyboardStroke)
+  final String svgAsset;
 
-  const TextFieldToggle({
-    super.key,
-    required this.svgAsset,
-  });
+  const TextFieldToggle({super.key, required this.svgAsset});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -31,7 +28,7 @@ class TextFieldToggle extends ConsumerWidget {
           color: isActive ? AppColors.primary : AppColors.secondary,
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.3),
+              color: Colors.black.withValues(alpha: 0.3),
               blurRadius: 4.0,
               offset: Offset(0, 2),
             ),
@@ -43,7 +40,9 @@ class TextFieldToggle extends ConsumerWidget {
             AnimatedAlign(
               duration: const Duration(milliseconds: 300),
               curve: Curves.easeInOut,
-              alignment: isActive ? Alignment.centerRight : Alignment.centerLeft,
+              alignment: isActive
+                  ? Alignment.centerRight
+                  : Alignment.centerLeft,
               child: Container(
                 width: 30.0,
                 height: 30.0,
@@ -52,7 +51,7 @@ class TextFieldToggle extends ConsumerWidget {
                   color: AppColors.onPrimary,
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.black.withOpacity(0.3),
+                      color: Colors.black.withValues(alpha: 0.3),
                       blurRadius: 4.0,
                       offset: Offset(0, 2),
                     ),
