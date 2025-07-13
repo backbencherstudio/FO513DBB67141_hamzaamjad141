535a58bda1a2ed2d2b62265115013ff08b023cea
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-07-13 16:48:05 +0600

    pull

--- lib/features/privacy_policy_terms_and_condition_screens/privacy_policy_screen/presentation/privacy_policy_screen.dart
+++ lib/features/privacy_policy_terms_and_condition_screens/privacy_policy_screen/presentation/privacy_policy_screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../../core/utils/common_widget/common_widget.dart';
 import '../../data/data.dart';
 
 class PrivacyPolicyScreen extends StatelessWidget {
@@ -21,14 +22,23 @@ class PrivacyPolicyScreen extends StatelessWidget {
           child: ListView(
             padding: EdgeInsets.zero,
             children: [
-              Align(
-                  alignment: Alignment.topLeft,
-                  child: IconButton(padding: EdgeInsets.zero, onPressed: ()=>context.pop(), icon: Icon(Icons.arrow_back_ios_new_outlined,),)),
-              SizedBox(height: 5.h,),
-              Align(
-                  alignment: Alignment.topLeft,
-                  child
-                  : Text("Privacy Policy",style: textTheme.headlineSmall,)),
+              /// Back button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                spacing: 20.w,
+                children: [
+                  CommonWidget.secondaryButton(
+                    onTap: () => context.pop(),
+                    child: Icon(Icons.arrow_back),
+                  ),
+                  Text("Privacy Policy",style: textTheme.headlineSmall,)
+                ],
+              ),
+              // SizedBox(height: 5.h,),
+              // Align(
+              //     alignment: Alignment.topLeft,
+              //     child
+              //     : Text("Privacy Policy",style: textTheme.headlineSmall,)),
               SizedBox(height: 20.h,),
               Text(
                 (privacyPolicy) ,
